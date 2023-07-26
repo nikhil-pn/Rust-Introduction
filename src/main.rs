a62a190893bca13bdf6e3c19ea3e4a1b3d461741
nikhilpn725 <nikhilpn725@gmail.com>
2023-07-27 01:24:49 +0530

    data types learned

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    //unsigned integer
+    let unsigned: u8 = 10;
+
+    //signed integer
+    let signed: u8 = 33;
+
+    //float used for decimal point
+    let float: f32 = 33.5;
+
+    //char
+    let letter: char = 'a';
+
+    //bool
+    let is_true: bool = true;
+
+    //array
+    let arr: [i32; 5] = [-1, 2, 3, 4, 5];
+
+    //tuples can store different types of data
+    let age: (i32, f64, u8) = (500, 6.4, 1);
+
+    let z = age.2;
+
+    println!("unsigned_interger{}", unsigned);
+    println!("signed_interger {}", signed);
+    println!("float {}", float);
+    println!("char {}", letter);
+    println!("istrue {}", is_true);
+    println!(" {}", arr[0]);
+    println!(" {}", arr[0]);
+    println!(" {}", z);
+}
